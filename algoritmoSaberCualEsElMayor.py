ee7ca4e02091a96a6dc51d02f8ab8075c4b0da94
DanielAcevedovega <antonydany236@gmail.com>
2024-02-23 17:45:08 -0500

    Especifique cual de los 3 numeros ingresados es el mayor

--- algoritmoSaberCualEsElMayor.py
+++ algoritmoSaberCualEsElMayor.py
@@ -5,12 +5,12 @@ num2 = input("Ingrese el numero 2: ");
 num3 = input("Ingrese el numero 3: ");
 
 if int(num1) > int(num2) and int(num1 > num3):
-    print("El numero mayor es el: "  + num1);
+    print("El numero mayor es el numero 1: "  + num1);
 
 elif int(num2) > int(num1) and int(num2) > int(num3):
-    print("El numero mayor es el: " + num2);
+    print("El numero mayor es el numero 2: " + num2);
 else:
-    print("El numero mayor es el: " + num3);
+    print("El numero mayor es el numero 3: " + num3);
      
 
 
