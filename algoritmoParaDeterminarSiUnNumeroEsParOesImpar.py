cdae273a040ca9644f3867672d29cb9a7d5a8646
DanielAcevedovega <antonydany236@gmail.com>
2024-02-23 18:20:51 -0500

    Ejercicio del algoritmo 6

---
+++ algoritmoParaDeterminarSiUnNumeroEsParOesImpar.py
@@ -0,0 +1,9 @@
+print("Bienvenido al programa para determinar si tu numero es par o impar");
+
+n = int(input("Pon tu numero favorito: "));
+
+if(n%2) == 0:
+    print("Tu numero favorito es un numero PAR");
+else:
+    print("Tu numero favorito es un numero IMPAR");
+
