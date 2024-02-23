48648516616dda6cafb1e80e1f66bbd5c279e80f
DanielAcevedovega <antonydany236@gmail.com>
2024-02-23 18:03:27 -0500

    Ejercicio del algoritmo 5

---
+++ algoritmoParaConvertirGradosCelsiusEnFahrenheit.py
@@ -0,0 +1,6 @@
+print("Este programa convertira los grados celsius en Fahrenheit")
+
+gradosCel = int(input("Introduzca la temperatura en celsius: "));
+
+gradosFah = int(gradosCel * 9/5)+32;
+print("La temperatura a Fahrenheit es de: ") + gradosFah;
