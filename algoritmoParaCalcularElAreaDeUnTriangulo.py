0b2336310517ab9fd31867504ce2495e2acfed6a
DanielAcevedovega <antonydany236@gmail.com>
2024-02-23 18:56:16 -0500

    Ejercicio del algoritmo 7

---
+++ algoritmoParaCalcularElAreaDeUnTriangulo.py
@@ -0,0 +1,8 @@
+print("Este programa te calula el area de un triangulo");
+
+base = float(input("Digite la cuanto es base del triangulo: "));
+altura = float(input("Digite la cuanto es la altura del triangulo: "));
+
+area = (base * altura)/2;   
+
+print("El area del triangulo es: "+str(area));
