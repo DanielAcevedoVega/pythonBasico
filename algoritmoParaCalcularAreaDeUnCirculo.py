3dcc901ff6810c7dd7bf3b176e01e0cb354e4377
DanielAcevedovega <antonydany236@gmail.com>
2024-02-23 21:16:27 -0500

    Algoritmo 11 solucion

---
+++ algoritmoParaCalcularAreaDeUnCirculo.py
@@ -0,0 +1,7 @@
+print("Programa para calcular el area de un circulo: ");
+
+radio = float(input("Escriba de cuanto es la radio del circulo: "));
+
+area = 3.1416 * radio**2
+
+print("El area del circulo es: "+str(area));
